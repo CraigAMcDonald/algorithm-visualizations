7b9075d23fe8b4a5a9c2d9594f328c7068c4200e
Craig A. McDonald <craig@mcdonald.codes>
2020-05-08 18:51:27 +0100

    Change language over to Java

---
+++ src/main/java/codes/mcdonald/craig/algovisual/App.java
@@ -0,0 +1,8 @@
+package codes.mcdonald.craig.algovisual;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    }
+}
---
+++ src/test/java/codes/mcdonald/craig/algovisual/AppTest.java
@@ -0,0 +1,20 @@
+package codes.mcdonald.craig.algovisual;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
