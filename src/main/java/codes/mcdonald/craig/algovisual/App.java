864e17b65037028111fd7788b07ade3b05611e46
Craig A. McDonald <craig@mcdonald.codes>
2020-10-12 23:42:00 +0100

    Swing chosen for UI

--- src/main/java/codes/mcdonald/craig/algovisual/App.java
+++ src/main/java/codes/mcdonald/craig/algovisual/App.java
@@ -1,8 +1,23 @@
 package codes.mcdonald.craig.algovisual;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-    }
+import javax.swing.JFrame;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+public class App {
+
+	public static void main(String[] args) {
+
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException e) {
+			// Don't care, just try and set the theme
+		}
+
+		JFrame frame = new JFrame("Algorithm Visualizations");
+		frame.setLocationRelativeTo(null);
+		frame.setSize(1024, 768);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
 }
